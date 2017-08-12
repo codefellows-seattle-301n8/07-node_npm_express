bc93242462f207e09f8e7fbbd5b1251f5ab1ba70
Kyle Richey <richey.kyle.i@gmail.com>
2017-08-12 14:36:31 -0700

    fixed console.log

--- starter-code/server.js
+++ starter-code/server.js
@@ -32,5 +32,5 @@ app.post('/articles', bodyParser, function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
-  console.log(PORT);
+  console.log('Our Server is running on this port: ' + PORT);
 });
