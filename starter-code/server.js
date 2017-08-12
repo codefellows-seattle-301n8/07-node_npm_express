3212a9ca63c74ae4fb416964ca82c2a0162ccc2d
jrzollin <jrzollin@yahoo.com>
2017-08-12 15:07:30 -0700

    last TODO no stretch

--- starter-code/server.js
+++ starter-code/server.js
@@ -10,10 +10,12 @@ const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
+var express = require('express');
+var app = express();
 app.use(express.static('public'));
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+app.use(express.static('public/', {index: 'new.html'}));
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -25,4 +27,5 @@ app.post('/articles', bodyParser, function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`Port Number: ${PORT}`);
 });
